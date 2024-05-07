88c5097b946f09667c6470fc3f83adcc07718b4d
princessetowe <princessetowe@gmail.com>
2024-05-07 13:03:58 +0100

    firstcommit

--- signup/views.py
+++ signup/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from models import Lecturer
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        faculty = request.POST['faculty']
+        department = request.POST['department']
+        course_title = request.POST['course_title']
+        course_code = request.POST['course_code']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if Lecturer.objects.filter(email=email).exists():
+                messages.info(request='email exists')
+            else:
+                user = Lecturer.objects()
