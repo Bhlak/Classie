1fe040064400fa11cb9a172d7b0165041b1e6f03
Bhlak <bhlakcodes@proton.me>
2024-06-13 10:34:17 +0100

    update

--- course_list/views.py
+++ course_list/views.py
@@ -53,7 +53,7 @@ class ClistAPIView(APIView):
                       {"course_title":"Bio-mechanics", "course_code":"BIM422", "year":"4", "faculty":"Faculty of Medical Sciences", "departments" :"Physiology", "dep_code": "PHYS"},
                       {"course_title":"Physiology2", "course_code":"PHG432", "year":"4", "faculty":"Faculty of Medical Sciences", "departments" :"Physiology", "dep_code": "PHYS"},
                       {"course_title":"Bio-mechanics2", "course_code":"BIM522", "year":"5", "faculty":"Faculty of Medical Sciences", "departments" :"Physiology", "dep_code": "PHYS"},
-                      {"course_title":"Bio-mechanics2", "course_code":"BIM522", "year":"5", "faculty":"Faculty of Medical Sciences", "departments" : "Anatomy", "dep_code": "ANAT"}
+                      {"course_title":"Bio-mechanics2", "course_code":"BIM522", "year":"5", "faculty":"Faculty of Medical Sciences", "departments" : "Anatomy", "dep_code": "ANAT"},
                       {"course_title":"Bio-mechanics3", "course_code":"BIM520", "year":"5", "faculty":"Faculty of Medical Sciences", "departments" :"Physiology", "dep_code": "PHYS"},
                       {"course_title":"Bio-mechanics3", "course_code":"BIM520", "year":"5", "faculty":"Faculty of Medical Sciences", "departments" : "Anatomy", "dep_code": "ANAT"}
                       ]
